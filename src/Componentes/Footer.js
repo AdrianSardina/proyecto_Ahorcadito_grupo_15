84af5bc418a66eed170ff6fdfef06b9559425f9d
LautaroGuardo <lautaro.guardo.99@gmail.com>
2022-10-07 17:47:33 -0300

    agrego el anterior trabajo, eliminando archivos que no usaremos, para fucionarlo con el nuevo, doy nuevo estilo a la pagina Mas Juegos, agrego pie de pagina, agrego nuevos botnoes necesarios para acceder al nuevo juego

---
+++ src/Componentes/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <>
+                <footer>
+                    <p>Creado con fines de estudio By GRUPO15</p>
+                    <div className="piePagina"></div>
+                </footer>
+            </>
+        );
+    }
+}
+export default Footer;
