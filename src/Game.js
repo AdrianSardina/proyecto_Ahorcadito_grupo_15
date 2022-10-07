fcc50a84af40a003da8ea78bf6cc3f022da77f0d
Sardina Jose Adrian <you@example.com>
2022-10-06 22:24:39 -0300

    Creacion del proyecto de Ahorcadito

---
+++ src/Game.js
@@ -0,0 +1,27 @@
+// import Boton from "./Componentes/Boton"
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { BrowserRouter, Routes, Route} from "react-router-dom";
+// import Masjuegos from "./Componentes/MasJuegos";
+// import NotFound from "./Componentes/NotFound";
+// import './css/estiloLG.css';
+// import Inicio from "./Componentes/Inicio";
+
+export default function Game()
+{
+    return(
+        <>
+        {/* <Boton></Boton>
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Inicio/>} />;
+        <Route path="/game" />;
+        <Route path="/nosotros" />;
+            <Route path="/masjuegos" element={<Masjuegos/>} />;
+            <Route path="*" element={<NotFound/>} />;
+        </Routes>
+        </BrowserRouter> */}
+        <h1>hola</h1>
+        </>
+        
+    )
+}
